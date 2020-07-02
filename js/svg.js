1ffff063210bfbdf57f7262b1873359b7cb6b9c6
kali65536 <zx18700839924@gmail.com>
2020-07-02 19:29:38 +0800

    Site updated: 2020-07-02 19:29:30

--- js/svg.js
+++ js/svg.js
@@ -1,3 +1,3 @@
-// build time:Thu Jul 02 2020 13:24:02 GMT+0800 (GMT+08:00)
+// build time:Thu Jul 02 2020 19:28:46 GMT+0800 (GMT+08:00)
 $(".card-info-social-icons").append('<a class="social-icon" href="https://y.qq.com/n/yqq/singer/003lLX8g0I6OHL.html" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-qqyinle"></use></svg></a>');
 //rebuild by neat 
